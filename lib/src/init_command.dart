720356fda07c1ef587297c7a15cf69c17c65539f
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-12-29 00:10:53 +0100

    updated rules version in init

--- lib/src/init_command.dart
+++ lib/src/init_command.dart
@@ -5,7 +5,7 @@ import 'package:args/args.dart';
 import 'package:path/path.dart' as path;
 import 'package:polymerize/package_graph.dart';
 
-const String RULES_VERSION = 'v_0_0_5';
+const String RULES_VERSION = 'v_0_0_6';
 
 Iterable<PackageNode> _transitiveDeps(PackageNode n, {Set<PackageNode> visited}) sync* {
   if (visited == null) {
