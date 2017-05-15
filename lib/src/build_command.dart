3f3bbe7fd6e34e1796e35a9e1195276dfef17aec
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-05-15 22:32:55 +0200

    added build command

--- lib/src/build_command.dart
+++ lib/src/build_command.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:args/args.dart';
+import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
 import 'package:polymerize/src/dep_analyzer.dart';
+
+Logger _logger = new Logger('polymerize');
 
 Future build(ArgResults command) async {
   String bower_resolutions_path = command['bower-resolutions'];
@@ -17,6 +20,8 @@ Future build(ArgResults command) async {
   } else {
     root = command.rest.single;
   }
+
+  _logger.info("Updating build files ...");
   WorkspaceBuilder builder = await WorkspaceBuilder.create(root, root,
                                                            dart_bin_path: dart_bin_path,
                                                            rules_version: rules_version,
@@ -27,5 +32,11 @@ Future build(ArgResults command) async {
 
   // Then run bazel
 
-  await Process.run('bazel',['build',':all']);
+  _logger.info("Running bazel ...");
+  Process bazel = await Process.start('bazel',['build',':all']);
+  stdout.addStream(bazel.stdout);
+  stderr.addStream(bazel.stderr);
+
+  await bazel.exitCode;
+
 }
